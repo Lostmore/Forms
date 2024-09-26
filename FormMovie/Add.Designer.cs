dd51b327ee643a5da60b956e1cb12e3623fee035
Lostmore <secondmyworld@gmail.com>
2024-09-26 22:35:15 +0300

    form "add" updated

--- FormMovie/Add.Designer.cs
+++ FormMovie/Add.Designer.cs
@@ -37,7 +37,6 @@
             this.label6 = new System.Windows.Forms.Label();
             this.label7 = new System.Windows.Forms.Label();
             this.label8 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.textBox4 = new System.Windows.Forms.TextBox();
             this.textBox5 = new System.Windows.Forms.TextBox();
@@ -50,6 +49,14 @@
             this.label9 = new System.Windows.Forms.Label();
             this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
             this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
@@ -132,13 +139,6 @@
             this.label8.TabIndex = 4;
             this.label8.Text = "Рейтинг";
             // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(4, 48);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(87, 20);
-            this.textBox1.TabIndex = 8;
-            // 
             // textBox2
             // 
             this.textBox2.Location = new System.Drawing.Point(108, 48);
@@ -183,11 +183,12 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 178);
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(4, 235);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(144, 38);
+            this.button1.Size = new System.Drawing.Size(49, 49);
             this.button1.TabIndex = 16;
-            this.button1.Text = "Добавить";
+            this.button1.Text = "+";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
@@ -200,35 +201,106 @@
             // 
             // textBox9
             // 
-            this.textBox9.Location = new System.Drawing.Point(174, 106);
+            this.textBox9.Location = new System.Drawing.Point(12, 106);
             this.textBox9.Multiline = true;
             this.textBox9.Name = "textBox9";
-            this.textBox9.Size = new System.Drawing.Size(253, 100);
+            this.textBox9.Size = new System.Drawing.Size(243, 97);
             this.textBox9.TabIndex = 19;
             // 
             // label9
             // 
             this.label9.AutoSize = true;
+            this.label9.BackColor = System.Drawing.SystemColors.Control;
+            this.label9.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.label9.Location = new System.Drawing.Point(270, 86);
+            this.label9.Location = new System.Drawing.Point(105, 86);
             this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(74, 17);
+            this.label9.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.label9.Size = new System.Drawing.Size(76, 19);
             this.label9.TabIndex = 20;
             this.label9.Text = "Описание";
             // 
             // dateTimePicker1
             // 
-            this.dateTimePicker1.Location = new System.Drawing.Point(447, 103);
+            this.dateTimePicker1.Location = new System.Drawing.Point(491, 103);
             this.dateTimePicker1.Name = "dateTimePicker1";
             this.dateTimePicker1.Size = new System.Drawing.Size(117, 20);
             this.dateTimePicker1.TabIndex = 21;
             // 
             // dateTimePicker2
             // 
-            this.dateTimePicker2.Location = new System.Drawing.Point(569, 103);
+            this.dateTimePicker2.Location = new System.Drawing.Point(614, 103);
             this.dateTimePicker2.Name = "dateTimePicker2";
             this.dateTimePicker2.Size = new System.Drawing.Size(117, 20);
             this.dateTimePicker2.TabIndex = 22;
+            // 
+            // button2
+            // 
+            this.button2.Image = ((System.Drawing.Image)(resources.GetObject("button2.Image")));
+            this.button2.Location = new System.Drawing.Point(99, 235);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(49, 49);
+            this.button2.TabIndex = 16;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(582, 142);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(133, 113);
+            this.pictureBox2.TabIndex = 24;
+            this.pictureBox2.TabStop = false;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(598, 258);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(104, 23);
+            this.button3.TabIndex = 25;
+            this.button3.Text = "Загрузить фото";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 47);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(79, 21);
+            this.comboBox1.TabIndex = 26;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label10.Location = new System.Drawing.Point(483, 78);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(125, 17);
+            this.label10.TabIndex = 5;
+            this.label10.Text = "Дата добавления";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label11.Location = new System.Drawing.Point(617, 78);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(118, 17);
+            this.label11.TabIndex = 5;
+            this.label11.Text = "Дата изменения";
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button4.Image = ((System.Drawing.Image)(resources.GetObject("button4.Image")));
+            this.button4.Location = new System.Drawing.Point(51, 235);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(49, 49);
+            this.button4.TabIndex = 16;
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // Add
             // 
@@ -236,12 +308,17 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(693, 228);
+            this.ClientSize = new System.Drawing.Size(740, 285);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.dateTimePicker2);
             this.Controls.Add(this.dateTimePicker1);
             this.Controls.Add(this.label9);
             this.Controls.Add(this.textBox9);
             this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button4);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox8);
             this.Controls.Add(this.textBox7);
@@ -249,9 +326,10 @@
             this.Controls.Add(this.textBox5);
             this.Controls.Add(this.textBox4);
             this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label6);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.label8);
             this.Controls.Add(this.label4);
@@ -260,7 +338,9 @@
             this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "Add";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Add";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -276,7 +356,6 @@
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.Label label8;
-        private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.TextBox textBox4;
         private System.Windows.Forms.TextBox textBox5;
@@ -289,5 +368,12 @@
         private System.Windows.Forms.Label label9;
         private System.Windows.Forms.DateTimePicker dateTimePicker1;
         private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Button button4;
     }
 }
--- FormMovie/Add.cs
+++ FormMovie/Add.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.Common;
 using System.Data.OleDb;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,31 +17,333 @@ namespace FormMovie
     {
         public static string connectString = "Provider = Microsoft.ACE.OLEDB.12.0;Data Source = 17..20.mdb";
         private OleDbConnection myConnection;
+
+        private string logFilePath = "log.txt";
+
         public Add()
         {
             InitializeComponent();
             myConnection = new OleDbConnection(connectString);
             myConnection.Open();
+
+
+            LoadMoviesToComboBox();
+            comboBox1.SelectedIndexChanged += comboBox1_SelectedIndexChanged;
         }
+
+
+
+
 
         private void button1_Click(object sender, EventArgs e)
         {
-            int kod = Convert.ToInt32(textBox1.Text);
+            if (!ValidateInputs()) return;
+
             String Name = textBox2.Text;
             int date = Convert.ToInt32(textBox3.Text);
-            String Author = textBox4.Text;
-            String Country = textBox5.Text;
-            String Genry = textBox6.Text;
-            String Description = textBox9.Text;
-            String rating = Convert.ToString(textBox7.Text);
-            String time = textBox8.Text;
-            string dateAdded = dateTimePicker1.Value.ToString("dd.MM.yyyy");
-            string dateEdit = dateTimePicker2.Value.ToString("dd.MM.yyyy");
-            string query = "INSERT INTO Movies (Movie_id, Film_name, Year_of_issue, Director, Description, Country, Genre, Rating, Duration, Date_added, Date_of_change) VALUES (" + kod + ",'" + Name + "','" + date + "','" + Author + "','" + Description + "','" + Country + "','" + Genry + "','" + rating + "','" + time + "','" + dateAdded + "','" + dateEdit + "')";
+
+            // Проверка, существует ли фильм в базе
+            if (CheckIfMovieExists(Name, date))
+            {
+                MessageBox.Show("Фильм уже существует в базе данных.");
+                return;
+            }
+
+            try
+            {
+                String Author = textBox4.Text;
+                String Country = textBox5.Text;
+                String Genry = textBox6.Text;
+                String Description = textBox9.Text;
+                String rating = Convert.ToString(textBox7.Text);
+                String time = textBox8.Text;
+                string dateAdded = dateTimePicker1.Value.ToString("dd.MM.yyyy");
+                string dateEdit = dateTimePicker2.Value.ToString("dd.MM.yyyy");
+
+                string query = "INSERT INTO Movies (Film_name, Year_of_issue, Director, Description, Country, Genre, Rating, Duration, Date_added, Date_of_change) " +
+                               "VALUES (@Name, @Year, @Author, @Description, @Country, @Genre, @Rating, @Duration, @DateAdded, @DateEdit)";
+
+                using (OleDbCommand command = new OleDbCommand(query, myConnection))
+                {
+                    command.Parameters.AddWithValue("@Name", Name);
+                    command.Parameters.AddWithValue("@Year", date);
+                    command.Parameters.AddWithValue("@Author", Author);
+                    command.Parameters.AddWithValue("@Description", Description);
+                    command.Parameters.AddWithValue("@Country", Country);
+                    command.Parameters.AddWithValue("@Genre", Genry);
+                    command.Parameters.AddWithValue("@Rating", rating);
+                    command.Parameters.AddWithValue("@Duration", time);
+                    command.Parameters.AddWithValue("@DateAdded", dateAdded);
+                    command.Parameters.AddWithValue("@DateEdit", dateEdit);
+
+                    command.ExecuteNonQuery();
+                }
+
+                MessageBox.Show("Данные о фильме добавлены.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка: " + ex.Message);
+            }
+            finally
+            {
+                this.Close();
+            }
+        }
+
+
+        private void ShowImage(string imagePath)
+        {
+            Form imageForm = new Form
+            {
+                StartPosition = FormStartPosition.CenterScreen,
+                Size = new Size(400, 400) // Размер окна
+            };
+
+            PictureBox pictureBox = new PictureBox
+            {
+                Image = new Bitmap(imagePath),
+                SizeMode = PictureBoxSizeMode.StretchImage,
+                Dock = DockStyle.Fill // Заполнение формы
+            };
+
+            imageForm.Controls.Add(pictureBox);
+            imageForm.ShowDialog(); // Модальное окно
+        }
+
+
+        private bool ValidateInputs()
+        {
+            if (string.IsNullOrWhiteSpace(textBox2.Text))
+            {
+                MessageBox.Show("Введите название фильма.");
+                return false;
+            }
+
+            if (!int.TryParse(textBox3.Text, out _))
+            {
+                MessageBox.Show("Введите корректное число.");
+                return false;
+            }
+
+            if (string.IsNullOrWhiteSpace(textBox4.Text))
+            {
+                MessageBox.Show("Введите имя режиссера.");
+                return false;
+            }
+
+            // Другие проверки для полей
+            return true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox2.Clear();
+            textBox3.Clear();
+            textBox4.Clear();
+            textBox5.Clear();
+            textBox6.Clear();
+            textBox7.Clear();
+            textBox8.Clear();
+            textBox9.Clear();
+            dateTimePicker1.Value = DateTime.Now;
+            dateTimePicker2.Value = DateTime.Now;
+            pictureBox2.Image = null;
+        }
+
+
+        private void Add_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (myConnection != null && myConnection.State == ConnectionState.Open)
+            {
+                myConnection.Close();
+                LogEvent("Соединение с базой данных закрыто.");
+            }
+        }
+
+        private void LoadMoviesToComboBox()
+        {
+            string query = "SELECT Movie_id, Film_name, Year_of_issue FROM Movies";
             OleDbCommand command = new OleDbCommand(query, myConnection);
-            command.ExecuteNonQuery();
-            MessageBox.Show("Данные о фильме добавлены.");
-            this.Close();
+            OleDbDataReader reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+                ComboBoxItem item = new ComboBoxItem
+                {
+                    Id = Convert.ToInt32(reader["Movie_id"]),
+                    Name = reader["Film_name"].ToString(),
+                    Year = Convert.ToInt32(reader["Year_of_issue"])
+                };
+                comboBox1.Items.Add(item);
+            }
+            reader.Close();
+        }
+
+
+        private void LoadMovieDetails(int movieId)
+        {
+            string query = "SELECT * FROM Movies WHERE Movie_id = @id";
+            OleDbCommand command = new OleDbCommand(query, myConnection);
+            command.Parameters.AddWithValue("@id", movieId);
+            OleDbDataReader reader = command.ExecuteReader();
+
+            if (reader.Read())
+            {
+                textBox2.Text = reader["Film_name"].ToString();
+                textBox3.Text = reader["Year_of_issue"].ToString();
+                textBox4.Text = reader["Director"].ToString();
+                textBox5.Text = reader["Country"].ToString();
+                textBox6.Text = reader["Genre"].ToString();
+                textBox7.Text = reader["Rating"].ToString();
+                textBox8.Text = reader["Duration"].ToString();
+                textBox9.Text = reader["Description"].ToString();
+
+                if (reader["Date_added"] != DBNull.Value)
+                {
+                    dateTimePicker1.Value = Convert.ToDateTime(reader["Date_added"]);
+                }
+
+                // Загрузка даты изменения (dateTimePicker2)
+                if (reader["Date_of_change"] != DBNull.Value)
+                {
+                    dateTimePicker2.Value = Convert.ToDateTime(reader["Date_of_change"]);
+                }
+
+            }
+            reader.Close();
+        }
+
+        private class ComboBoxItem
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public int Year { get; set; }
+            public string Director { get; set; }
+            public string Country { get; set; }
+            public string Genre { get; set; }
+            public string Rating { get; set; }
+
+            public override string ToString() => $"{Name} ({Year})"; // Используем имя и год для отображения
+        }
+
+        private void LogEvent(string message)
+        {
+            using (StreamWriter writer = new StreamWriter(logFilePath, true))
+            {
+                writer.WriteLine($"{DateTime.Now}: {message}");
+            }
+        }
+
+
+
+        private bool CheckIfMovieExists(string name, int year)
+        {
+            string query = $"SELECT COUNT(*) FROM Movies WHERE Film_name = @name AND Year_of_issue = @year";
+            OleDbCommand command = new OleDbCommand(query, myConnection);
+            command.Parameters.AddWithValue("@name", name);
+            command.Parameters.AddWithValue("@year", year);
+
+            try
+            {
+                int count = (int)command.ExecuteScalar();
+                return count > 0;
+            }
+            catch (Exception ex)
+            {
+                LogError("Ошибка при проверке существования фильма: " + ex.Message);
+                MessageBox.Show("Произошла ошибка при проверке фильма. Подробности сохранены в лог.");
+                return true;  // Предполагаем, что фильм существует, если произошла ошибка
+            }
+        }
+
+        private void LogError(string errorMessage)
+        {
+            string logFilePath = "error_log.txt";
+            using (StreamWriter writer = new StreamWriter(logFilePath, true))
+            {
+                writer.WriteLine($"{DateTime.Now}: {errorMessage}");
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = "C:\\";
+                openFileDialog.Filter = "Image files (*.jpg, *.jpeg, *.png, *.bmp)|*.jpg;*.jpeg;*.png;*.bmp";
+                openFileDialog.Title = "Выберите изображение";
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    // Загружаем изображение в PictureBox
+                    pictureBox2.Image = new Bitmap(openFileDialog.FileName);
+                }
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedItem != null)
+            {
+                ComboBoxItem selectedMovie = (ComboBoxItem)comboBox1.SelectedItem;
+                LoadMovieDetails(selectedMovie.Id);
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (!ValidateInputs()) return;
+
+            if (comboBox1.SelectedItem != null)
+            {
+                ComboBoxItem selectedMovie = (ComboBoxItem)comboBox1.SelectedItem;
+
+                string name = textBox2.Text;
+                int year = Convert.ToInt32(textBox3.Text);
+                string director = textBox4.Text;
+                string country = textBox5.Text;
+                string genre = textBox6.Text;
+                string rating = textBox7.Text;
+                string duration = textBox8.Text;
+                string description = textBox9.Text;
+                string dateAdded = dateTimePicker1.Value.ToString("dd.MM.yyyy");
+                string dateEdit = dateTimePicker2.Value.ToString("dd.MM.yyyy");
+
+                string query = "UPDATE Movies SET Film_name = @Name, Year_of_issue = @Year, Director = @Director, " +
+                               "Description = @Description, Country = @Country, Genre = @Genre, Rating = @Rating, " +
+                               "Duration = @Duration, Date_added = @DateAdded, Date_of_change = @DateEdit " +
+                               "WHERE Movie_id = @MovieId";
+
+                using (OleDbCommand command = new OleDbCommand(query, myConnection))
+                {
+                    command.Parameters.AddWithValue("@Name", name);
+                    command.Parameters.AddWithValue("@Year", year);
+                    command.Parameters.AddWithValue("@Director", director);
+                    command.Parameters.AddWithValue("@Description", description);
+                    command.Parameters.AddWithValue("@Country", country);
+                    command.Parameters.AddWithValue("@Genre", genre);
+                    command.Parameters.AddWithValue("@Rating", rating);
+                    command.Parameters.AddWithValue("@Duration", duration);
+                    command.Parameters.AddWithValue("@DateAdded", dateAdded);
+                    command.Parameters.AddWithValue("@DateEdit", dateEdit);
+                    command.Parameters.AddWithValue("@MovieId", selectedMovie.Id);
+
+                    try
+                    {
+                        command.ExecuteNonQuery();
+                        MessageBox.Show("Данные о фильме успешно обновлены.");
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Ошибка при обновлении данных: " + ex.Message);
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Выберите фильм для редактирования.");
+            }
         }
 
     }
